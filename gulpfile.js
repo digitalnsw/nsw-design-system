880b27100d74f768caab8854a11f92d1652f4d2d
Sean Elliott <>
2019-10-31 11:48:21 +1100

    GDS-495 - templating set up - added metalsmith as a build engine, providing the ability to create a static website of the components - create a basic config for metalsmith to achieve what we need for now to be built upon - added in basic handlebars helpers - added example component of `buttons` this has the structure we will be using for our components, featuring styles, js, html (hbs), model - created a basic layout wrapper for our templates

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,24 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const sourcemaps = require('gulp-sourcemaps');
-const browsersync = require('browser-sync').create();
+const browsersync = require('browser-sync');
+const server = browsersync.create();
 const surge = require('gulp-surge');
 const sassLint = require('gulp-sass-lint');
 const svgSprite = require('gulp-svg-sprite');
 const del = require('del');
 const postcssNormalize = require('postcss-normalize');
-var sassGlob = require('gulp-sass-glob');
+const sassGlob = require('gulp-sass-glob');
+const Metalsmith = require('metalsmith');
+const handlebars = require('handlebars');
+const layouts     = require('metalsmith-layouts');
+const inplace = require('metalsmith-in-place');
+const collections = require('metalsmith-collections');
+const ignore = require('metalsmith-ignore');
+const discoverPartials = require('metalsmith-discover-partials');
+const dataLoader = require('metalsmith-data-loader');
+const debug = require('metalsmith-debug-ui');
+const discoverHelpers = require('metalsmith-discover-helpers');
 
 sass.compiler = require('node-sass');
 
@@ -51,11 +62,57 @@ const htmlConfig = {
   build : dir.build
 }
 
+const metalsmithConfig = {
+  src : dir.src,
+  watch : dir.src + '**/*.hbs',
+  build : dir.build,
+  metadata: {
+    title: "Digital NSW Design System",
+    description: "Design system for Digital NSW",
+    generator: "Metalsmith",
+    url: ""
+  },
+  ignoreFiles: [
+    'assets/**/*',
+    '**/*.scss',
+    '**/*.js',
+    '**/*.json',
+    '**/*.DS_Store',
+    'global/**/*',
+    'patterns/**/*',
+  ],
+  helpers: {
+    directory: './src/global/helpers',
+    pattern: /\.js$/
+  },
+  partials: {
+    directory: './src/',
+    pattern: /\.hbs$/
+  },
+  data: {
+    dataProperty: 'model',
+  },
+  collection: {
+    components: ['components/**/*.hbs', '!components/**/_*.hbs'],
+  },
+  inplace: {
+    pattern: '**/*.hbs',
+    rename: true,
+    engine: 'handlebars',
+  },
+  layouts: {
+    engine: 'handlebars',
+    rename: true,
+    directory: './src/global/layouts',
+    default: 'layout.hbs',
+    pattern: '**/*.{hbs,md,html}'
+  }
+}
+
 function compileSvg() {
   return src(svgConfig.src)
     .pipe(svgSprite(svgConfig.svgSprite)).on('error', function(error){ console.log(error); })
     .pipe(dest(svgConfig.build))
-    .pipe(browsersync.reload({ stream: true }));
 }
 
 function buildStyles() {
@@ -66,7 +123,6 @@ function buildStyles() {
     .pipe(postcss(postcssProcessors))
     .pipe(sourcemaps.write())
     .pipe(dest(scssConfig.build))
-    .pipe(browsersync.reload({ stream: true }));
 }
 
 function lintStyles(){
@@ -79,23 +135,20 @@ function lintStyles(){
 function copyHtml() {
   return src(htmlConfig.src)
     .pipe(dest(htmlConfig.build))
-    .pipe(browsersync.reload({ stream: true }));
-}
-
-function watchFiles(done) {
-  watch(scssConfig.watch, styles);
-  watch(htmlConfig.watch, copyHtml);
-  watch(svgConfig.watch, compileSvg);
-  done();
 }
 
 function browserSync(done) {
-  browsersync.init({
+  server.init({
     server: {
        baseDir: dir.build,
        index: 'index.html'
     }
   })
+  done();
+}
+
+function reload(done) {
+  server.reload();
   done();
 }
 
@@ -111,9 +164,63 @@ function cleanUp() {
     return del(dir.build + '**', {force:true});
 }
 
+function cleanBuild(files, metalsmith, done) {
+	const fileNames = Object.keys(files);
+  fileNames.forEach(function (path) {
+		const file = path.split('.');
+		if(file[0].indexOf('assets') > -1 || file[0].indexOf('global') > -1 || file[0].indexOf('partials') > -1){
+			delete files[path];
+		}
+	});
+	done(null, files);
+};
+
+function metalsmithBuild(callback) {
+  const metalsmith = new Metalsmith(__dirname);
+  //debug.patch(metalsmith);
+  metalsmith.metadata(metalsmithConfig.metadata)
+  metalsmith.source(metalsmithConfig.src)
+  metalsmith.destination(metalsmithConfig.build)
+  metalsmith.use(
+    ignore(metalsmithConfig.ignoreFiles)
+  )
+  metalsmith.clean(false)
+  metalsmith.use(
+    discoverHelpers(metalsmithConfig.helpers)
+  )
+  metalsmith.use(
+    discoverPartials(metalsmithConfig.partials)
+  )
+  metalsmith.use(
+    dataLoader(metalsmithConfig.data)
+  )
+  metalsmith.use(
+    collections(metalsmithConfig.collection)
+  )
+  metalsmith.use(
+    inplace(metalsmithConfig.inplace)
+  )
+  metalsmith.use(
+    layouts(metalsmithConfig.layouts)
+  )
+  metalsmith.use(cleanBuild);
+  metalsmith.build(function(err, files) {
+    if (err) { throw err; }
+    callback();
+  });
+}
+
 const styles = series(lintStyles, buildStyles);
 const build = series(cleanUp, copyHtml, styles, compileSvg, surgeDeploy);
-const dev = series(copyHtml, styles, compileSvg, watchFiles, browserSync)
+const dev = series(copyHtml, metalsmithBuild, styles, compileSvg, watchFiles, browserSync)
+
+function watchFiles(done) {
+  watch(scssConfig.watch, series(styles, reload));
+  watch(htmlConfig.watch, series(copyHtml, reload));
+  watch(svgConfig.watch, series(compileSvg, reload));
+  watch(metalsmithConfig.watch, series(metalsmithBuild, reload));
+  done();
+}
 
 // Export commands.
 exports.scss = buildStyles; // $ gulp sass - compiles the sass
@@ -121,5 +228,6 @@ exports.watch = watchFiles; // $ gulp watch - watches the files
 exports.lint = lintStyles; // $ gulp lint - lints the sass
 exports.svg = compileSvg; // $ gulp lint - lints the sass
 exports.build = build; // $ gulp build - builds the files
-exports.clean = cleanUp; // $ gulp build - clean the dist directory
+exports.clean = cleanUp; // $ gulp clean - clean the dist directory
+exports.metal = metalsmithBuild;
 exports.default = dev; // $ gulp - default gulp task
