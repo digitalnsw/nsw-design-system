6455145dde99eabba2fd3e0d4c28c673901e3da4
Anna Yeshtukova <>
2022-06-14 16:30:16 +1000

    Adding accessibility features

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -4,9 +4,13 @@ class Dialog {
     this.dialogWrapper = element.querySelector('.nsw-dialog__wrapper')
     this.openBtn = document.querySelectorAll('.js-open-dialog-'.concat(element.getAttribute('id')))
     this.closeBtn = element.querySelectorAll('.js-close-dialog')
+    // eslint-disable-next-line max-len
+    this.focusableEls = element.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])')
+    this.body = document.body
     this.openEvent = (e) => this.openDialog(e)
     this.closeEvent = (e) => this.closeDialog(e)
     this.clickEvent = (e) => this.clickDialog(e)
+    this.trapEvent = (e) => this.trapFocus(e)
   }
 
   init() {
@@ -24,22 +28,32 @@ class Dialog {
     if (this.dialog.classList.contains('js-dialog-dismiss')) {
       this.dialog.addEventListener('click', this.clickEvent, false)
     }
+
+    this.focusableEls[this.focusableEls.length - 1].addEventListener('blur', this.trapEvent, false)
   }
 
   openDialog() {
     this.dialog.setAttribute('aria-expanded', 'true')
     this.dialog.classList.add('active')
+    this.body.classList.add('dialog-active')
+    this.focusableEls[0].focus()
   }
 
   closeDialog() {
     this.dialog.setAttribute('aria-expanded', 'false')
     this.dialog.classList.remove('active')
+    this.body.classList.remove('dialog-active')
   }
 
   clickDialog(e) {
     if (!this.dialogWrapper.contains(e.target)) {
       this.closeDialog()
     }
+  }
+
+  trapFocus(e) {
+    e.preventDefault()
+    this.focusableEls[0].focus()
   }
 }
 
