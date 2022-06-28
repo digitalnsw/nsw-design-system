4740e4ee60d96b4f560b33c208b5d4d2cacdff6d
Lauren Hitchon <lauren@hitchon.me>
2022-06-28 15:30:00 +1000

    collapse

--- src/components/filters/filters.js
+++ src/components/filters/filters.js
@@ -52,3 +52,28 @@ class Filters {
 }
 
 export default Filters
+
+
+const triggers = Array.from(document.querySelectorAll('.nsw-filters__collapse button'))
+
+const fnmap = {
+  toggle: 'toggle',
+  show: 'add',
+  hide: 'remove',
+}
+
+const collapse = (selector, cmd) => {
+  const targets = Array.from(document.querySelectorAll(selector))
+  targets.forEach((target) => {
+    target.classList[fnmap[cmd]]('show')
+  })
+}
+
+window.addEventListener('click', (ev) => {
+  const elm = ev.target
+  if (triggers.includes(elm)) {
+    const selector = elm.getAttribute('data-target')
+    collapse(selector, 'toggle')
+    elm.classList.toggle('active')
+  }
+}, false)
