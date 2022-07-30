53b3838d96ebcdbf00787b4e7e07ff7a56620f1c
Lauren Hitchon <lauren@hitchon.me>
2022-07-30 14:43:58 +1000

    updated filters components

--- src/components/filters/filters.js
+++ src/components/filters/filters.js
@@ -1,7 +1,9 @@
+import { uniqueId } from '../../global/scripts/helpers/utilities'
+
 class Filters {
   constructor(element) {
     this.filters = element
-    this.openButton = element.querySelector('.nsw-filters__controls button')
+    this.openButtons = element.querySelectorAll('.nsw-filters__controls button')
     this.closeButtons = element.querySelectorAll('.nsw-filters__back button')
     this.all = element.querySelectorAll('.nsw-filters__all')
     this.allBlocks = Array.prototype.slice.call(this.all)
@@ -10,29 +12,68 @@ class Filters {
     this.hideEvent = (e) => this.hideFilters(e)
     this.showMoreEvent = (e) => this.showMore(e)
     this.body = document.body
+    this.accordionButtons = element.querySelectorAll('.nsw-filters__item-button')
+    this.buttons = []
+    this.content = []
+    this.toggleEvent = (e) => this.toggle(e)
   }
 
   init() {
+    this.setUpDom()
     this.controls()
   }
 
-  controls() {
-    this.openButton.addEventListener('click', this.showEvent, false)
+  setUpDom() {
+    this.filters.classList.add('ready')
 
-    this.closeButtons.forEach((element) => {
-      element.addEventListener('click', this.hideEvent, false)
+    this.accordionButtons.forEach((button) => {
+      const buttonElem = button
+      const uID = uniqueId('accordion')
+      buttonElem.setAttribute('type', 'button')
+      buttonElem.setAttribute('aria-expanded', 'false')
+      buttonElem.setAttribute('aria-controls', uID)
+
+      const contentElem = buttonElem.nextElementSibling
+      contentElem.id = buttonElem.getAttribute('aria-controls')
+      contentElem.hidden = true
+
+      this.content.push(contentElem)
+      this.buttons.push(buttonElem)
     })
+  }
+
+  controls() {
+    if (this.openButtons) {
+      this.openButtons.forEach((element) => {
+        element.addEventListener('click', this.showEvent, false)
+      })
+    }
+    if (this.closeButtons) {
+      this.closeButtons.forEach((element) => {
+        element.addEventListener('click', this.hideEvent, false)
+      })
+    }
 
     this.all.forEach((element) => {
       const showMoreButton = element.nextElementSibling
       showMoreButton.addEventListener('click', this.showMoreEvent, false)
     })
+
+    if (this.buttons) {
+      this.buttons.forEach((element) => {
+        element.addEventListener('click', this.toggleEvent, false)
+      })
+    }
   }
 
   showFilters(e) {
     e.preventDefault()
-    this.filters.classList.add('active')
-    this.body.classList.add('filters-open')
+    if (this.filters.classList.contains('nsw-filters--down')) {
+      this.filters.classList.toggle('active')
+    } else {
+      this.filters.classList.add('active')
+      this.body.classList.add('filters-open')
+    }
   }
 
   hideFilters(e) {
@@ -48,6 +89,37 @@ class Filters {
     const currentAll = this.allBlocks[currentIndex]
     currentAll.classList.remove('hidden')
     currentShowMore.classList.add('hidden')
+  }
+
+  getTargetContent(element) {
+    const currentIndex = this.buttons.indexOf(element)
+    return this.content[currentIndex]
+  }
+
+  setAccordionState(element, state) {
+    const targetContent = this.getTargetContent(element)
+
+    if (state === 'open') {
+      element.classList.add('active')
+      element.setAttribute('aria-expanded', 'true')
+      targetContent.hidden = false
+    } else if (state === 'close') {
+      element.classList.remove('active')
+      element.setAttribute('aria-expanded', 'false')
+      targetContent.hidden = true
+    }
+  }
+
+  toggle(e) {
+    const { currentTarget } = e
+    const targetContent = this.getTargetContent(currentTarget)
+    const isHidden = targetContent.hidden
+
+    if ((isHidden)) {
+      this.setAccordionState(currentTarget, 'open')
+    } else {
+      this.setAccordionState(currentTarget, 'close')
+    }
   }
 }
 
