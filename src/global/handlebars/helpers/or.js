48e86d77e9c672546fc362883b31b7f3b2e608a0
Lauren Hitchon <lauren@hitchon.me>
2022-08-16 09:25:40 +1000

    Renamed variables and updating naming structure of.json files

--- src/global/handlebars/helpers/or.js
+++ src/global/handlebars/helpers/or.js
@@ -1,3 +1,16 @@
-module.exports = function () {
-  return Array.prototype.slice.call(arguments, 0, -1).some(Boolean)
+function isObject(thing) {
+  return (typeof thing === 'object');
+}
+
+module.exports = function(...params) {
+  if (isObject(params[params.length - 1])) {
+    params.pop();
+  }
+
+  for (let i = 0; i < params.length; i++) {
+    if (params[i]) {
+      return true;
+    }
+  }
+  return false;
 }
