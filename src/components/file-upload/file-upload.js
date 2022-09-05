20aa99ff605a26a35bfcb6e04bf600e41c09af9f
Lauren Hitchon <lauren@hitchon.me>
2022-09-05 10:15:34 +1000

    Files created

---
+++ src/components/file-upload/file-upload.js
@@ -0,0 +1,15 @@
+class FileUpload {
+  constructor(element) {
+    this.fileUpload = element
+  }
+
+  init() {
+    this.controls()
+  }
+
+  controls() {
+    this.controls()
+  }
+}
+
+export default FileUpload
