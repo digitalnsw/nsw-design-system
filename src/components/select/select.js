be7309b20c1a95cef9b79065de353f496d5c7893
Lauren Hitchon <lauren@hitchon.me>
2022-09-08 09:25:47 +1000

    Update select.js

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -1,6 +1,11 @@
 class Select {
   constructor(element) {
-    this.select = element
+    this.element = element
+    this.select = this.element.querySelectorAll('select')
+    this.optGroups = this.select.querySelectorAll('optgroup')
+    this.options = this.select.querySelectorAll('option')
+    this.selectId = this.select.getAttribute('id')
+    this.selectedOptCounter = 0
   }
 
   init() {
