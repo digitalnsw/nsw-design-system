13713ea9332ad6a00447364f3ec6ca1955185994
laurenhitchon <lauren@hitchon.me>
2023-07-26 02:13:56 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@58b312504fa9286161027aa73df2d756dad52f21 🚀

--- js/main.js
+++ js/main.js
@@ -182,7 +182,6 @@
     handleOutsideClick(event) {
       const isOutsideNav = !this.mainNavElement.contains(event.target);
       if (isOutsideNav) {
-        this.saveElements(event);
         this.toggleSubNavDesktop(true);
         event.preventDefault();
       }
