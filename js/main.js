f860e6e6cd822e8b1c6524d534e1fface0734760
laurenhitchon <lauren@hitchon.me>
2023-11-20 00:44:18 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@b20281dbbe3166d26f8d6e3819e7f33093fd9e76 🚀

--- js/main.js
+++ js/main.js
@@ -3411,6 +3411,127 @@
     }
   }
 
+  class BackTop {
+    constructor(element) {
+      this.element = element;
+      this.dataElement = this.element.getAttribute('data-element');
+      this.scrollOffset = this.element.getAttribute('data-offset');
+      this.text = false;
+      this.icon = false;
+      this.scrollElement = this.dataElement ? document.querySelector(this.dataElement) : window;
+      this.scrollPosition = 0;
+      this.width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+      this.height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+      this.condition = false;
+    }
+    init() {
+      this.createButton();
+      this.element.addEventListener('click', event => {
+        event.preventDefault();
+        if (!window.requestAnimationFrame) {
+          this.scrollElement.scrollTo(0, 0);
+        } else if (this.dataElement) {
+          this.scrollElement.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        } else {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        }
+      });
+      this.checkBackToTop();
+      const debounceEvent = this.debounce(this.checkBackToTop);
+      this.scrollElement.addEventListener('scroll', () => {
+        debounceEvent();
+      });
+      const debounceResize = this.debounce(this.resizeHandler);
+      window.addEventListener('resize', () => {
+        debounceResize();
+      });
+    }
+    createButton() {
+      const textSpan = this.constructor.createElement('span');
+      const iconSpan = this.constructor.createElement('span', ['material-icons', 'nsw-material-icons'], {
+        title: 'Back to top',
+        focusable: 'false',
+        'aria-hidden': 'true'
+      });
+      this.element.append(textSpan, iconSpan);
+      this.text = this.element.querySelector('span:not(.material-icons)');
+      this.icon = this.element.querySelector('span.material-icons');
+      this.createButtonContent();
+    }
+    createButtonContent() {
+      if (this.width < 768) {
+        this.text.innerText = 'Top';
+        this.icon.innerText = 'keyboard_arrow_up';
+      } else {
+        this.text.innerText = 'Back to top';
+        this.icon.innerText = 'north';
+      }
+    }
+    checkBackToTop() {
+      let windowTop = this.scrollElement.scrollTop || document.documentElement.scrollTop;
+      if (!this.dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+      const scroll = this.scrollPosition;
+      this.scrollPosition = window.scrollY;
+      if (this.scrollOffset && this.scrollOffset > 0) {
+        this.condition = windowTop >= this.scrollOffset;
+        this.element.classList.toggle('active', this.condition);
+      } else {
+        this.condition = scroll > this.scrollPosition && this.scrollPosition > 200;
+        this.element.classList.toggle('active', this.condition);
+      }
+    }
+    resizeHandler() {
+      const oldWidth = this.width;
+      const oldHeight = this.height;
+      this.width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+      this.height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+      if (oldWidth !== this.width && oldHeight === this.height) {
+        this.createButtonContent();
+      }
+    }
+    debounce(fn) {
+      var _this = this;
+      let wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 250;
+      let timeout;
+      return function () {
+        for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+          args[_key] = arguments[_key];
+        }
+        const context = _this;
+        if (!window.requestAnimationFrame) {
+          clearTimeout(timeout);
+          timeout = setTimeout(() => fn.apply(context, args), wait);
+        } else {
+          if (timeout) {
+            window.cancelAnimationFrame(timeout);
+          }
+          timeout = window.requestAnimationFrame(() => {
+            fn.apply(context, args);
+          });
+        }
+      };
+    }
+    static createElement(tag) {
+      let classes = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+      let attributes = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+      const element = document.createElement(tag);
+      if (classes.length > 0) {
+        element.classList.add(...classes);
+      }
+      Object.entries(attributes).forEach(_ref => {
+        let [key, value] = _ref;
+        element.setAttribute(key, value);
+      });
+      return element;
+    }
+  }
+
   /* eslint-disable max-len */
   if (window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = Array.prototype.forEach;
@@ -3446,6 +3567,7 @@
     const toggletip = document.querySelectorAll('.js-toggletip');
     const link = document.querySelectorAll('.js-link');
     const popover = document.querySelectorAll('.js-popover');
+    const backTop = document.querySelectorAll('.js-back-to-top');
     if (openSearchButton) {
       openSearchButton.forEach(element => {
         new SiteSearch(element).init();
@@ -3510,9 +3632,15 @@
         new Popover(element).init();
       });
     }
+    if (backTop) {
+      backTop.forEach(element => {
+        new BackTop(element).init();
+      });
+    }
   }
 
   exports.Accordion = Accordion;
+  exports.BackTop = BackTop;
   exports.Dialog = Dialog;
   exports.ExternalLink = ExternalLink;
   exports.FileUpload = FileUpload;
