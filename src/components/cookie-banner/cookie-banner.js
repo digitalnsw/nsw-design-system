f64ad930fae2a09817d41d45f1daf41ddf9bd8c6
Jim Carlton <imucup@gmail.com>
2024-03-27 17:24:19 +1100

    Initial setup of Cookie banner.

---
+++ src/components/cookie-banner/cookie-banner.js
@@ -0,0 +1,21 @@
+class CookieBanner {
+  constructor(element) {
+    this.messageElement = element
+    this.closeButton = element.querySelector('.js-close-alert')
+    this.closeMessageEvent = (e) => this.closeMessage(e)
+  }
+
+  init() {
+    this.controls()
+  }
+
+  controls() {
+    this.closeButton.addEventListener('click', this.closeMessageEvent, false)
+  }
+
+  closeMessage() {
+    this.messageElement.hidden = true
+  }
+}
+
+export default CookieBanner
