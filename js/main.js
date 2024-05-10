fae66811dab88faf05ec44ba2676431a51d67f64
laurenhitchon <lauren@hitchon.me>
2024-05-10 04:19:01 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@900b5a65867482ee0fb25b0c66bad7b2b6e1a623 🚀

--- js/main.js
+++ js/main.js
@@ -4698,8 +4698,9 @@
         }
       });
       this.share.addEventListener('click', event => {
-        event.preventDefault();
         const button = event.target.closest('a');
+        if (!button) return;
+        event.preventDefault();
         const social = button.getAttribute('data-social');
         const url = this.getSocialUrl(button, social);
         if (social === 'mail') {
