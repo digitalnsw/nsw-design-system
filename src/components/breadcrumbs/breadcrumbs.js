4f2993d20473e3c5fb6f3c0da43b03d21bf0291c
Jim Carlton <imucup@gmail.com>
2024-11-12 16:42:40 +1100

    Breadcrumbs updates to add in js for ellipsis

---
+++ src/components/breadcrumbs/breadcrumbs.js
@@ -0,0 +1,40 @@
+class Breadcrumbs {
+  constructor(element) {
+    this.element = element
+    this.allBreadcrumbs = this.element.querySelector('.nsw-breadcrumbs ol')
+    this.secondBreadcrumb = this.element.querySelector('.js-breadcrumbs li:nth-child(2)')
+    this.breadcrumbs = document.querySelectorAll('.js-breadcrumbs li')
+    this.condition = false
+  }
+
+  init() {
+    this.createToggle()
+
+    this.element.addEventListener('click', (event) => {
+      event.preventDefault()
+      this.allBreadcrumbs.classList.toggle('nsw-breadcrumbs__show-all')
+    })
+  }
+
+  createToggle() {
+    const toggle = this.constructor.createElement('li', ['nsw-breadcrumbs__show-more-toggle'])
+    toggle.innerHTML = '<button aria-label="Show more breadcrumbs" class="nsw-breadcrumbs__toggle-button" type="button">…</button>'
+
+    this.allBreadcrumbs.insertBefore(toggle, this.secondBreadcrumb)
+  }
+
+  static createElement(tag, classes = [], attributes = {}) {
+    const element = document.createElement(tag)
+    if (classes.length > 0) {
+      element.classList.add(...classes)
+    }
+
+    Object.entries(attributes).forEach(([key, value]) => {
+      element.setAttribute(key, value)
+    })
+
+    return element
+  }
+}
+
+export default Breadcrumbs
