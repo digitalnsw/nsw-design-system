1416ea2028e98e4374702ef854da74360232745f
oisa <oisa@users.noreply.github.com>
2025-11-12 00:33:48 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@be58efe508527fd298933db65eaac05de797be8a 🚀

--- cookie-consent-config.js
+++ cookie-consent-config.js
@@ -1,4 +1,4 @@
-/*! NSW Design System v3.22.10 | MIT License */
+/*! NSW Design System v3.22.11 | MIT License */
 (function (factory) {
   typeof define === 'function' && define.amd ? define('NSW', factory) :
   factory();
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*! NSW Design System v3.22.10 | MIT License */
+/*! NSW Design System v3.22.11 | MIT License */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define('NSW', ['exports'], factory) :
@@ -5942,4 +5942,4 @@
 
 }));
 
-;(function(g){try{g.NSW=g.NSW||{};g.NSW.VERSION="3.22.10";}catch(e){} }(typeof globalThis!=='undefined'?globalThis:(typeof window!=='undefined'?window:self)));
+;(function(g){try{g.NSW=g.NSW||{};g.NSW.VERSION="3.22.11";}catch(e){} }(typeof globalThis!=='undefined'?globalThis:(typeof window!=='undefined'?window:self)));
