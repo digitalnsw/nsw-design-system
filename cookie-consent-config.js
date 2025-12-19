17bf5a7f909a5fccdba62afc528010a9f490880c
oisa <75308491+oisa@users.noreply.github.com>
2025-12-19 08:08:02 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@2464d238e54a136fc5947b3d5ef66255a167fb39 🚀

--- cookie-consent-config.js
+++ cookie-consent-config.js
@@ -1,4 +1,4 @@
-/*! NSW Design System v3.24.0 | MIT License */
+/*! NSW Design System v3.23.0 | MIT License */
 (function (factory) {
   typeof define === 'function' && define.amd ? define('NSW', factory) :
   factory();
