4afa8abbd33b7cc5ccc9e3d5358281af05841546
oisa <75308491+oisa@users.noreply.github.com>
2026-02-13 06:11:36 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@fe304198c37cdea7492ad9870a370b1ef926075d 🚀

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*! NSW Design System v3.24.2 | MIT License */
+/*! NSW Design System v3.24.3 | MIT License */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define('NSW', ['exports'], factory) :
@@ -6853,4 +6853,4 @@
 
 }));
 
-;(function(g){try{g.NSW=g.NSW||{};g.NSW.VERSION="3.24.2";}catch(e){} }(typeof globalThis!=='undefined'?globalThis:(typeof window!=='undefined'?window:self)));
+;(function(g){try{g.NSW=g.NSW||{};g.NSW.VERSION="3.24.3";}catch(e){} }(typeof globalThis!=='undefined'?globalThis:(typeof window!=='undefined'?window:self)));
