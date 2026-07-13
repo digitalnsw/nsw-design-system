8664eba5c050c7639f4fddba7d8c93f50dc0f471
oisa <75308491+oisa@users.noreply.github.com>
2026-07-13 04:27:28 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@8d6a2589cff8d3fe1918b2aeafd6113329e0fde7 🚀

--- js/components/quick-exit/quick-exit.d.ts
+++ js/components/quick-exit/quick-exit.d.ts
@@ -2,9 +2,8 @@ export default class QuickExit {
     /**
      * Enhance or create a Quick Exit inside the sticky container
      */
-    static init({ safeUrl, description, enableEsc, enableCloak, focusFirst, }?: {
+    static init({ safeUrl, enableEsc, enableCloak, focusFirst, }?: {
         safeUrl?: string | undefined;
-        description?: string | undefined;
         enableEsc?: boolean | undefined;
         enableCloak?: boolean | undefined;
         focusFirst?: boolean | undefined;
@@ -12,10 +11,11 @@ export default class QuickExit {
     /**
      * Build minimal, no-JS friendly markup
      */
-    static buildMarkup({ description, safeUrl, }: {
-        description: any;
+    static buildMarkup({ enableEsc, safeUrl, }: {
+        enableEsc: any;
         safeUrl: any;
     }): HTMLAnchorElement;
+    static buildKeyboardHint(id?: string): HTMLSpanElement;
     /**
      * Progressive enhancement (click logic, keyboard, cloak, focus)
      */
@@ -26,11 +26,17 @@ export default class QuickExit {
         focusFirst: any;
     }): void;
     static bindDoubleEsc(callback: any): void;
-    static ensureSrOnlyMessage(): void;
+    static ensureSrOnlyMessage(enableEsc?: boolean): void;
+    static updatePageTitle(): void;
     static applyCloak(): void;
     static focusFirst(node: any): void;
+    static getOptionsFromElement(root: any): {
+        safeUrl: any;
+        enableEsc: any;
+        enableCloak: any;
+    };
     /**
-     * Enhance any existing QE in the container (no declarative parsing).
+     * Enhance any existing server-rendered Quick Exit elements.
      */
     static autoInit(): void;
 }
