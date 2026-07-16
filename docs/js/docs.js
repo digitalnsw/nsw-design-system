67dfd61e75be333c1739c79c4f7c73c71363b2de
oisa <75308491+oisa@users.noreply.github.com>
2026-07-16 06:01:07 +0000

    Deploying to gh-pages from @ digitalnsw/nsw-design-system@a34ebe33bec9b2fa43a62fecdc25b2fef84323f3 🚀

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -257,8 +257,13 @@
   }, {
     label: 'Our ecosystem',
     template: 'result',
-    keywords: 'Digital Visual Identity, Core styles and components, NSW Design System Community, Built in accessibility, UX, content guidance, UI, code starter kits',
+    keywords: 'Digital Visual Identity, Core styles and components, Community and support, Built in accessibility, UX, content guidance, UI, code starter kits',
     url: '/docs/content/about/our-ecosystem.html'
+  }, {
+    label: 'NSW Design System Community archive',
+    template: 'result',
+    keywords: 'NSW Design System Community, Community forum, Discourse, Community archived, Community archive, Former Community, community.designsystem.nsw.gov.au',
+    url: '/community-archived/index.html'
   }, {
     label: 'Release notes',
     template: 'result',
