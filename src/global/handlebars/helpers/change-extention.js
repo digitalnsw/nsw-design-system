9b3902ebf793991ebf0f7ec95fb05e2a9b28588c
Jim Carlton <imucup@gmail.com>
2026-09-24 17:13:55 +1000

    Updates to main page template, introduces classic structure with breadcrumbs

--- src/global/handlebars/helpers/change-extention.js
+++ src/global/handlebars/helpers/change-extention.js
@@ -7,6 +7,8 @@ module.exports = function (page, option, id) {
       return '/' + page.split('index.')[0] + 'theme.html'
     case 'demo':
       return '/' + page.split('index.')[0] + 'demo.html'
+    case 'index':
+      return '/' + page.split('/').slice(0, -1).join('/') + '/index.html'
     case 'anchor':
       return '/' + page.split('.')[0] + '.html#' + id
     default:
